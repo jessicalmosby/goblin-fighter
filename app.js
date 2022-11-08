1ccfe55a711e17f5a00d30f9bbc744bf65dc6ca3
Jessica Mosby <jessicamosby@mac.com>
2022-11-08 15:05:12 -0500

    added state and event listener for submit button as well as display goblin function

--- app.js
+++ app.js
@@ -7,11 +7,48 @@ const adventurerHPEl = document.querySelector('#adventurer-hp');
 const adventurerImgEl = document.querySelector('#fin');
 const adventurerImg2El = document.querySelector('#jake');
 const goblinListEl = document.querySelector('.goblins');
+const form = document.querySelector('form');
 
 /* State */
+let defeatedGoblinCount = 0;
+let playerHP = 10;
+let goblins = [
+    { id: 1, name: 'Grobyn', hp: 1 },
+    { id: 2, name: 'Goblin King', hp: 4 },
+];
+let currentId = 3;
 
 /* Events */
+form.addEvenListener('sumbit', (e) => {
+    e.preventDefault();
+    const data = new FormData(form);
+    const goblinName = data.get('goblin-name');
+
+    const newGoblin = {
+        id: currentId,
+        name: goblinName,
+        hp: Math.ceil(Math.random() * 5),
+    };
+    currentId++;
+
+    goblins.push(newGoblin);
+    displayGoblins();
+});
 
 /* Display Functions */
+function displayGoblins() {
+    goblinListEl.textContent = '';
+
+    for (let goblin of goblins) {
+        const goblinEl = renderGoblin(goblin);
+
+        goblinEl.addEventListener('click', () => {
+            goblinClickHandler(goblin);
+        });
+
+        goblinListEl.append(goblinEl);
+    }
+}
+displayGoblins();
 
 // (don't forget to call any display functions you want to run on page load!)
