76cd7c19fd2dc53efc2cc97648766fe169f6300c
Jessica Mosby <jessicamosby@mac.com>
2022-11-08 14:35:59 -0500

    finished renderGoblin function and added DOM elements

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 /* Imports */
+import { renderGoblin } from './render.utils.js';
 
 /* Get DOM Elements */
+const defeatedNumberEl = document.querySelector('#defeated-number');
+const adventurerHPEl = document.querySelector('#adventurer-hp');
+const adventurerImgEl = document.querySelector('#fin');
+const adventurerImg2El = document.querySelector('#jake');
+const goblinListEl = document.querySelector('.goblins');
 
 /* State */
 
--- render.utils.js
+++ render.utils.js
@@ -5,4 +5,19 @@ export function renderGoblin(goblinData) {
     const hpEl = document.createElement('p');
 
     goblinEl.classList.add('goblin');
+
+    nameEl.textContent = goblinData.name;
+    hpEl.id = `goblin-hp-${goblinData.id}`;
+    hpEl.textContent = goblinData.hp < 0 ? 0 : goblinData.hp;
+
+    faceEl.id = `goblin-face-${goblinData.id}`;
+    faceEl.textContent = goblinData.hp > 0 ? '😈' : '🔥';
+
+    if (goblinData.hp < 0) {
+        goblinEl.classList.add('dead');
+    }
+
+    goblinEl.append(nameEl, faceEl, hpEl);
+
+    return goblinEl;
 }
