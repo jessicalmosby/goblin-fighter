2c46926c5b1e401144c3d75efdd6383b76deb193
Jessica Mosby <jessicamosby@mac.com>
2022-11-08 13:40:34 -0500

    started renderGoblin function

---
+++ render.utils.js
@@ -0,0 +1,8 @@
+export function renderGoblin(goblinData) {
+    const goblinEl = document.createElement('div');
+    const faceEl = document.createElement('p');
+    const nameEl = document.createElement('p');
+    const hpEl = document.createElement('p');
+
+    goblinEl.classList.add('goblin');
+}
